8848fa9707da0af35152a9adae0abb0423cdad11
Jagerheat <u.sergeyvladimirovich@gmail.com>
2021-05-10 04:51:37 +0930

    Step

--- Py_Project/Lesson_3/task_1.py
+++ Py_Project/Lesson_3/task_1.py
@@ -1,5 +1,5 @@
-strl = input("Get string:")
-if len(strl) < 2:
+some_stl = input("Get string:")
+if len(some_stl) < 2:
  	print(None)
 else:
-	print("Take string:", (strl[0:2] + strl[-2:]
+	print("Take string:", (some_stl[0:2] + some_stl[-2:])
