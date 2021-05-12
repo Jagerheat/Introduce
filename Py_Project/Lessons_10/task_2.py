47e8dcb7fa88eef2517775b093ce6feb4f04da95
Jagerheat <u.sergeyvladimirovich@gmail.com>
2021-05-13 04:48:46 +0930

    Step

---
+++ Py_Project/Lessons_10/task_2.py
@@ -0,0 +1,21 @@
+
+class Dog:
+
+	age_factors = 7;
+
+	def __init__(self):
+
+		enter_age = 0
+		input('vvedite', enter_age)
+
+	human_age = int(input("Input a dog's age in human years: "))
+d_age = 7
+if human_age < 0:
+	print("Bad idea")
+	exit()
+else:
+	human_age <= 2
+	age = d_age * human_age
+	print("The dog's age in human years is", age)
+
+
